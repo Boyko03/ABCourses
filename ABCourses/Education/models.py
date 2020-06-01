f55affdadb97d730eddbcf9fb88f012d0194fa32
Ann <krastanova98@gmail.com>
2020-06-01 17:29:36 +0300

    tried email and password,

--- ABCourses/Education/models.py
+++ ABCourses/Education/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class BaseUser(models.Model):
     user_id = models.AutoField(primary_key=True, unique=True)
     user_name = models.CharField(max_length=250, unique=True)
+    user_email = models.EmailField(max_length=255)
+    user_password = models.TextField()
 
     def __str__(self):
         return f'User {self.user_name}'
